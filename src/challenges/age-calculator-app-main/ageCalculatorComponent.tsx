d3f5f9eb7c0105e26e1d285547e365e532afd9ac
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-09-04 22:11:03 +0100

    updated age challenge

--- src/challenges/age-calculator-app-main/ageCalculatorComponent.tsx
+++ src/challenges/age-calculator-app-main/ageCalculatorComponent.tsx
@@ -4,21 +4,36 @@
 
 function AgeCalculatorComponent() {
 	return (
-		<div className="w-80 p-10 rounded-br-3xl rounded-2xl   bg-white flex">
-			<form>
-				<label htmlFor="day" className="">
-					<span>DAY</span>
-					<input placeholder="DD" name="day" type="number" className="block p-2 border rounded-md w-20 font-bold"/>
+		<div className="w-96 p-6 rounded-xl rounded-br-[100px] bg-white flex flex-col">
+			<form className=" text-sm w-80">
+				<label htmlFor="day" className="inline-block">
+					<span className="text-xs">DAY</span>
+					<input placeholder="DD" name="day" required type="text" className="block p-2 border rounded-md w-20 font-bold mr-2 cursor-pointer focus-visible:border-purple-600"/>
 				</label>
-				<label htmlFor="day" className="">
-					<span>MONTH</span>
-					<input placeholder="MM" name="day" type="number" className="block p-2 border rounded-md w-20 font-bold"/>
+				<label htmlFor="day" className="inline-block">
+					<span className="text-xs">MONTH</span>
+					<input placeholder="MM" name="months" required type="text" className="block p-2 border rounded-md w-20 font-bold mr-2"/>
 				</label>
-				<label htmlFor="day" className="">
-					<span>YEAR</span>
-					<input placeholder="YYYY" name="day" type="number" className="block p-2 border rounded-md w-20 font-bold"/>
+				<label htmlFor="day" className="inline-block">
+					<span className="text-xs">YEAR</span>
+					<input placeholder="YYYY" name="years" required type="text" className="block p-2 border rounded-md w-20 font-bold"/>
 				</label>
+				<div className="flex items-center mt-1">
+					<hr className="mt-5 flex-grow h-5 self-center"/>
+					<button type="submit"><img src="/age-calculator/icon-arrow.svg" width={"45px"} className="bg-purple-700 p-3 rounded-full cursor-pointer" /></button>
+				</div>
 			</form>
+			<div className="block">
+				<span className="text-purple-700 font-black text-6xl italic mr-2">--</span><span className="text-5xl font-black italic">years</span>
+			</div>
+			
+			<div className="block">
+				<span className="text-purple-700 font-black text-6xl italic mr-2">3</span><span className="text-5xl font-black italic">months</span>
+			</div>
+			
+			<div className="block">
+				<span className="text-purple-700 font-black text-6xl italic mr-2">26</span><span className="text-5xl font-black italic">days</span>
+			</div>
 		</div>
 	)
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import Rating from "./challenges/interactive-rating-component-main/rating-component.tsx"
-// import PinPadComponent from './challenges/pin-pad/pin-pad-component.tsx'
 import AgeCalculatorComponent from './challenges/age-calculator-app-main/ageCalculatorComponent.tsx'
+import Rating from "./challenges/interactive-rating-component-main/rating-component.tsx"
+import PinPadComponent from './challenges/pin-pad/pin-pad-component.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
