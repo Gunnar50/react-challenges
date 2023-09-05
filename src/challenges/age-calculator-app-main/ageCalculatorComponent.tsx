dc3ae80bdfb9b306c78004db98f2d34d8e2ec765
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-09-05 17:57:50 +0100

    updated challenge

--- src/challenges/age-calculator-app-main/ageCalculatorComponent.tsx
+++ src/challenges/age-calculator-app-main/ageCalculatorComponent.tsx
@@ -1,30 +1,50 @@
+import { useState } from "react";
 
+interface DateType {
+	day: string;
+	month: string;
+	year: string;
+}
 
 
 
 function AgeCalculatorComponent() {
+	const [date, setDate] = useState<DateType>({day: "", month: "", year: ""});
+	const [isSubmitted, setIsSubmitted] = useState<boolean>(false);
+
+	const changeValue = (e: React.ChangeEvent<HTMLInputElement>, name: string, value: string) => {
+		e.preventDefault();
+		setDate(prev => ({...prev, [name]: value}));
+	};
+
+	const handleSubmit = () => {
+		setIsSubmitted(true);
+	};
+
 	return (
 		<div className="w-96 p-6 rounded-xl rounded-br-[100px] bg-white flex flex-col">
 			<form className=" text-sm w-80">
 				<label htmlFor="day" className="inline-block">
 					<span className="text-xs">DAY</span>
-					<input placeholder="DD" name="day" required type="text" className="block p-2 border rounded-md w-20 font-bold mr-2 cursor-pointer focus-visible:border-purple-600"/>
+					<input onChange={(e) => changeValue(e, e.target.name, e.target.value)} placeholder="DD" name="day" required type="text" className="block p-2 border rounded-md w-20 font-bold mr-2 cursor-pointer focus-visible:border-purple-600"/>
 				</label>
 				<label htmlFor="day" className="inline-block">
 					<span className="text-xs">MONTH</span>
-					<input placeholder="MM" name="months" required type="text" className="block p-2 border rounded-md w-20 font-bold mr-2"/>
+					<input onChange={(e) => changeValue(e, e.target.name, e.target.value)} placeholder="MM" name="month" required type="text" className="block p-2 border rounded-md w-20 font-bold mr-2"/>
 				</label>
 				<label htmlFor="day" className="inline-block">
 					<span className="text-xs">YEAR</span>
-					<input placeholder="YYYY" name="years" required type="text" className="block p-2 border rounded-md w-20 font-bold"/>
+					<input onChange={(e) => changeValue(e, e.target.name, e.target.value)} placeholder="YYYY" name="year" required type="text" className="block p-2 border rounded-md w-20 font-bold"/>
 				</label>
 				<div className="flex items-center mt-1">
 					<hr className="mt-5 flex-grow h-5 self-center"/>
-					<button type="submit"><img src="/age-calculator/icon-arrow.svg" width={"45px"} className="bg-purple-700 p-3 rounded-full cursor-pointer" /></button>
+					<button onSubmit={handleSubmit} type="submit"><img src="/age-calculator/icon-arrow.svg" width={"45px"} className="bg-purple-700 p-3 rounded-full cursor-pointer" /></button>
 				</div>
 			</form>
 			<div className="block">
-				<span className="text-purple-700 font-black text-6xl italic mr-2">--</span><span className="text-5xl font-black italic">years</span>
+				<span className="text-purple-700 font-black text-6xl italic mr-2">
+						
+				</span><span className="text-5xl font-black italic">years</span>
 			</div>
 			
 			<div className="block">
